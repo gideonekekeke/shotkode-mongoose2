07696455f9193154b26b4e711ec94286217d42f3
gideonekekeke <71523437+gideonekekeke@users.noreply.github.com>
2021-05-07 14:37:19 +0100

    okay

--- MVC/SchemaData.js
+++ MVC/SchemaData.js
@@ -3,20 +3,20 @@ const mongoose = require("mongoose");
 const mySchema = mongoose.Schema({
   name: {
     type: String,
-    required: true,
+  
   },
   location: {
     type: String,
-    required: true,
+   
   },
   contact: {
     type: Number,
-    required: true,
+
   },
 
   Picture: {
     type: String,
-    required: true,
+   
   },
 
   // cover: {
